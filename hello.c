368efc4f39062187f4ee86880afe958b66fac0e2
박현재/NE <hyeonjae.park@nhnent.com>
2016-01-21 18:26:27 +0900

    aa

--- hello.c
+++ hello.c
@@ -6,9 +6,9 @@ int main(int argc, char *argv[]) {
   printf("hello world\n");
   return 0;
 }
-////    ////
-// end line
-//
-//
+////    //////
+// end line //
+//          //
+//          //
 ////    ////
  
